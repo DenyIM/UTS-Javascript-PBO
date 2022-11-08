93504a15b9f0ee76125310c52b66e22fa91e2f15
Deny Iqbal Mubarok <101798303+DenyIM@users.noreply.github.com>
2022-11-08 11:37:24 +0700

    Update Karyawan.java

--- Karyawan.java
+++ Karyawan.java
@@ -16,6 +16,14 @@ public class Karyawan {
         return totLembur;
     }
 
+    public int hitung(int var1, int var2){
+        return var1+var2;
+    }
+
+    public int hitung(int var1, int var2, int var3){
+        return var1+var2+var3;
+    }
+
     public String getNama() {
         return nama;
     }
